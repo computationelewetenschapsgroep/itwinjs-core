d75c77d9242a23ac351e0a79dbfb3b463e304fbb
Aniket Mitra <aniket.mitra.partner@decathlon.com>
2024-12-31 23:00:14 +0100

    Add snippet to query imodel
    Signed-off-by: Aniket Mitra <aniket.mitra.partner@decathlon.com>

---
+++ scripts/snippets/query_imodel.js
@@ -0,0 +1,23 @@
+"use strict";
+
+
+const core_backend_1 = await import("@itwin/core-backend");
+const core_backend_2 = await import("@itwin/core-bentley");
+
+await core_backend_1.IModelHost.startup({ profileName: "query-imodel" });
+
+const iModel = core_backend_1.StandaloneDb.openFile("/app/imodels/empty.bim", core_backend_2.OpenMode.Readonly);
+
+
+
+iModel.withPreparedStatement(`SELECT count(*) FROM bis.Element
+  `, (stmt) => {
+  console.log(stmt.getValue(0).getInteger());
+});
+
+iModel.withPreparedStatement(`SELECT ECInstanceId, Model, CodeValue, Parent  FROM bis.Element
+`, (stmt) => {
+  while (stmt.step() === core_backend_2.DbResult.BE_SQLITE_ROW) {
+    console.log(stmt.getRow());
+  }
+});
